2ba8e4349a130c4b4d8fa5992adcd1d23871626e
Jovana Hurra <jovana.hurra@gmail.com>
2020-11-11 10:13:25 +0100

    Uppdatering av UTF-8 för Javadoc

--- src/RollDice.java
+++ src/RollDice.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class RollDice {
+/**
+ * Created by: Jovana Hurra
+ * Date: 2020-11-10
+ * Project: Inlämningsuppgift 3
+ */
+
+public class RollDice {
+
+    /**
+     * En metod för tärningkast
+     *
+     * @param numberOfFaces typ av tärning
+     * @param numberOfRolls antal tärningar
+     * @return dice
+     */
+    public static int rollDice(int numberOfFaces, int numberOfRolls) {
+        double dice = 0;
+        for (int i = 0; i < numberOfRolls; i++) {
+            dice += (Math.random() * numberOfFaces) + 1;
+        }
+        return (int) dice;
+    }
 }
